cfca99ee53e016be7f58da07b59256ccd998f366
Eranosyan Eduard <EranosyanEduard@yandex.ru>
2025-09-25 08:43:28 +0000

    refactor: обобщить код класса, представляющего веб-компонент

---
+++ src/core/ui/define_component/Component.ts
@@ -0,0 +1,145 @@
+import { isObject } from 'es-toolkit/compat'
+import mapValues from 'es-toolkit/compat/mapValues'
+import { type html, render } from 'lit-html'
+import { reactive, watchEffect } from '../../reactivity'
+import type { Getter } from '../../typedef'
+import { type CurrentInstance, setCurrentInstance } from '../current_instance'
+import Prop from './Prop'
+import type { ComponentOptions } from './typedef'
+
+class Component<
+    Props extends Record<string, unknown> = Record<string, unknown>,
+    Emits extends string = string
+  >
+  extends HTMLElement
+  implements CurrentInstance
+{
+  readonly $options: CurrentInstance['$options']
+
+  private readonly options: ComponentOptions<Props, Emits>
+
+  private readonly root: this | ShadowRoot
+
+  private readonly template: Getter<ReturnType<typeof html>>
+
+  /** created */
+  constructor(options: ComponentOptions<Props, Emits>) {
+    super()
+    this.options = options
+    this.$options = {
+      hooks: {
+        onBeforeMount: new Set(),
+        onBeforeUpdate: new Set(),
+        onMounted: new Set(),
+        onUnmounted: new Set(),
+        onUpdated: new Set()
+      }
+    }
+    this.root = this.defineRoot()
+    this.template = this.defineTemplate()
+    this.$options.hooks.onMounted.add(this.defineRender.bind(this))
+    this.useLifecycleHooks({ hook: 'onBeforeMount' })
+  }
+
+  /** mounted */
+  connectedCallback(): void {
+    this.useLifecycleHooks({ hook: 'onMounted' })
+  }
+
+  /** destroyed */
+  disconnectedCallback(): void {
+    this.useLifecycleHooks({ hook: 'onUnmounted' })
+  }
+
+  private defineProps(): Props {
+    const props = reactive({})
+    Object.defineProperties(
+      this,
+      mapValues(this.options.props, (propOptions, propName) => {
+        const prop = new Prop(propName, propOptions)
+        const valueOrDefault = (propValue: unknown): Props[keyof Props] => {
+          // @ts-expect-error проигнорировать ошибку типизации:
+          // невозможно гарантировать, что значение propValue
+          // соответствует возвращаемому типу функции, но в данном
+          // случае это не повлияет на корректность работы кода.
+          return propValue ?? prop.options.default()
+        }
+        return {
+          get: () => {
+            // @ts-expect-error проигнорировать ошибку типизации:
+            // propName станет ключом props при выполнении программы.
+            return valueOrDefault(props[propName])
+          },
+          set: (propValue: unknown) => {
+            const propValue_ = valueOrDefault(propValue)
+            if (prop.options.validator(propValue_)) {
+              // @ts-expect-error проигнорировать ошибку типизации:
+              // см. комментарий в методе `get`.
+              props[propName] = propValue_
+            }
+          }
+        }
+      })
+    )
+    // @ts-expect-error проигнорировать ошибку типизации:
+    // код метода должен гарантировать соответствие значения
+    // props возвращаемому типу.
+    return props
+  }
+
+  private defineRender(): void {
+    let rendered = false
+    watchEffect(() => {
+      if (rendered) {
+        this.useLifecycleHooks({
+          clear: false,
+          hook: 'onBeforeUpdate'
+        })
+      }
+      render(this.template(), this.root)
+      if (rendered) {
+        this.useLifecycleHooks({
+          clear: false,
+          hook: 'onUpdated'
+        })
+      }
+      rendered = true
+    })
+  }
+
+  private defineRoot(): this | ShadowRoot {
+    return isObject(this.options.shadowRootConfig)
+      ? this.attachShadow(this.options.shadowRootConfig)
+      : this
+  }
+
+  private defineTemplate(): Getter<ReturnType<typeof html>> {
+    setCurrentInstance(this)
+    try {
+      return this.options.setup(this.defineProps(), {
+        emit: this.emit.bind(this)
+      })
+    } finally {
+      setCurrentInstance(null)
+    }
+  }
+
+  private emit(eventType: Emits, detail?: unknown) {
+    if (!this.options.emits?.includes(eventType)) return
+    this.dispatchEvent(new CustomEvent(eventType, { detail }))
+  }
+
+  private useLifecycleHooks(args: {
+    readonly clear?: boolean
+    readonly hook: keyof CurrentInstance['$options']['hooks']
+  }) {
+    const { clear = true, hook } = args
+    const hooks = this.$options.hooks[hook]
+    hooks.forEach((it) => {
+      it()
+    })
+    if (clear) hooks.clear()
+  }
+}
+
+export default Component
--- src/core/ui/define_component/define_component.ts
+++ src/core/ui/define_component/define_component.ts
@@ -1,148 +1,19 @@
-import isObject from 'es-toolkit/compat/isObject'
 import kebabCase from 'es-toolkit/compat/kebabCase'
-import mapValues from 'es-toolkit/compat/mapValues'
-import { render } from 'lit-html'
-import { reactive, watchEffect } from '../../reactivity'
-import { type CurrentInstance, setCurrentInstance } from '../current_instance'
-import Prop from './Prop'
-import type { ComponentOptions, PropsOptions } from './typedef'
+import Component from './Component'
+import type { ComponentOptions } from './typedef'
 
 function defineComponent<
   Props extends Record<string, unknown> = Record<string, unknown>,
   Emits extends string = string
 >(options: ComponentOptions<Props, Emits>): CustomElementConstructor {
-  const {
-    name,
-    emits = [] as [],
-    props: propsOptions = {} as PropsOptions<Props>,
-    setup,
-    shadowRootConfig = null
-  } = options
-  const Component = class extends HTMLElement implements CurrentInstance {
-    readonly $options: CurrentInstance['$options']
-
-    private readonly root: this | ShadowRoot
-
-    private readonly template: ReturnType<typeof setup>
-
-    /** created */
+  const { name } = options
+  const Component_ = class extends Component<Props, Emits> {
     constructor() {
-      super()
-      this.$options = {
-        hooks: {
-          onBeforeMount: new Set(),
-          onBeforeUpdate: new Set(),
-          onMounted: new Set(),
-          onUnmounted: new Set(),
-          onUpdated: new Set()
-        }
-      }
-      this.root = this.defineRoot()
-      this.template = this.defineTemplate()
-      this.$options.hooks.onMounted.add(this.defineRender.bind(this))
-      this.useLifecycleHooks({ hook: 'onBeforeMount' })
-    }
-
-    /** mounted */
-    connectedCallback(): void {
-      this.useLifecycleHooks({ hook: 'onMounted' })
-    }
-
-    /** destroyed */
-    disconnectedCallback(): void {
-      this.useLifecycleHooks({ hook: 'onUnmounted' })
-    }
-
-    private defineProps(): Props {
-      const props = reactive({})
-      Object.defineProperties(
-        this,
-        mapValues(propsOptions, (propOptions, propName) => {
-          const prop = new Prop(propName, propOptions)
-          const valueOrDefault = (propValue: unknown): Props[keyof Props] => {
-            // @ts-expect-error проигнорировать ошибку типизации:
-            // невозможно гарантировать, что значение propValue
-            // соответствует возвращаемому типу функции, но в данном
-            // случае это не повлияет на корректность работы кода.
-            return propValue ?? prop.options.default()
-          }
-          return {
-            get: () => {
-              // @ts-expect-error проигнорировать ошибку типизации:
-              // propName станет ключом props при выполнении программы.
-              return valueOrDefault(props[propName])
-            },
-            set: (propValue: unknown) => {
-              const propValue_ = valueOrDefault(propValue)
-              if (prop.options.validator(propValue_)) {
-                // @ts-expect-error проигнорировать ошибку типизации:
-                // см. комментарий в методе `get`.
-                props[propName] = propValue_
-              }
-            }
-          }
-        })
-      )
-      // @ts-expect-error проигнорировать ошибку типизации:
-      // код метода должен гарантировать соответствие значения
-      // props возвращаемому типу.
-      return props
-    }
-
-    private defineRender(): void {
-      let rendered = false
-      watchEffect(() => {
-        if (rendered) {
-          this.useLifecycleHooks({
-            clear: false,
-            hook: 'onBeforeUpdate'
-          })
-        }
-        render(this.template(), this.root)
-        if (rendered) {
-          this.useLifecycleHooks({
-            clear: false,
-            hook: 'onUpdated'
-          })
-        }
-        rendered = true
-      })
-    }
-
-    private defineRoot(): this | ShadowRoot {
-      return isObject(shadowRootConfig)
-        ? this.attachShadow(shadowRootConfig)
-        : this
-    }
-
-    private defineTemplate(): ReturnType<typeof setup> {
-      setCurrentInstance(this)
-      try {
-        return setup(this.defineProps(), { emit: this.emit.bind(this) })
-      } finally {
-        setCurrentInstance(null)
-      }
-    }
-
-    private emit(eventType: Emits, detail?: unknown) {
-      if (!emits.includes(eventType)) return
-      this.dispatchEvent(new CustomEvent(eventType, { detail }))
-    }
-
-    private useLifecycleHooks(args: {
-      readonly clear?: boolean
-      readonly hook: keyof CurrentInstance['$options']['hooks']
-    }) {
-      const { clear = true, hook } = args
-      const hooks = this.$options.hooks[hook]
-      hooks.forEach((it) => {
-        it()
-      })
-      if (clear) hooks.clear()
+      super(options)
     }
   }
-  customElements.define(kebabCase(name), Component)
-  return Component
+  customElements.define(kebabCase(name), Component_)
+  return Component_
 }
 
 export default defineComponent
